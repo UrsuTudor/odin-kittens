4cd89509a673e07093db028eee607a5205005f3f
UrsuTudor <tudor_ursu90@yahoo.com>
2024-10-27 14:04:55 +0200

    Added the other methods with corresponding views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -10,6 +10,7 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
+      flash[:success] = "Wooo, you added a new kitten!"
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
@@ -17,15 +18,25 @@ class KittensController < ApplicationController
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
   end
-
-  def destroy
-  end
-
   def edit
+    @kitten = Kitten.find(params[:id])
+    flash[:success] = "Nice! Your kitten will now be ready to apply for new jobs!"
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    @kitten.update!(kitten_params)
+
+    redirect_to @kitten
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:success] = "Awww, we're sorry to see it go! :("
+    redirect_to root_url, status: :see_other
   end
 
   private
